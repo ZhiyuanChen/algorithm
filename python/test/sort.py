6f61bc6b5f058c63cfda60f77d97f33b4251542d
Zhiyuan Chen <zc@int.ac.cn>
2019-11-06 02:56:36 +1100

    Print time that algorithm used

--- python/test/sort.py
+++ python/test/sort.py
@@ -1,5 +1,6 @@
 import unittest
 import random
+from time import time
 from python.sort import insertion, merge, heap, quick, test
 
 
@@ -12,24 +13,32 @@ class SortTest(unittest.TestCase):
     return result
 
   def setUp(self):
-    self.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    self.array = [i for i in range(1000)]
     self.test_list = [self.shuffle(self.array) for _ in range(100)]
 
   def testInsertion(self):
+    t = time()
     for l in self.test_list:
       assert self.array == insertion.sort(l)
+    print(time() - t)
 
   def testMerge(self):
+    t = time()
     for l in self.test_list:
       assert self.array == merge.sort(l)
+    print(time() - t)
 
   def testHeap(self):
+    t = time()
     for l in self.test_list:
       assert self.array == heap.sort(l)
+    print(time() - t)
 
   def testQuick(self):
+    t = time()
     for l in self.test_list:
       assert self.array == quick.sort(l)
+    print(time() - t)
 
   def testTest(self):
     for l in self.test_list:
