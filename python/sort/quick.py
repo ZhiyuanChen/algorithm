afbfc28e441d2706db15364e0f4f1f724291fbe2
Zhiyuan Chen <zc@int.ac.cn>
2019-11-06 03:49:39 +1100

    rename parameter

--- python/sort/quick.py
+++ python/sort/quick.py
@@ -1,24 +1,24 @@
 from random import randint
 
 
-def partition(array, start, stop):
+def partition(array, start, end):
   # Partition using Lomuto's partition Scheme.
   index = start - 1
-  for i in range(start, stop):
-    if array[i] < array[stop]:
+  for i in range(start, end):
+    if array[i] < array[end]:
       index += 1
       array[i], array[index] = array[index], array[i]
-  array[index + 1], array[stop] = array[stop], array[index + 1]
+  array[index + 1], array[end] = array[end], array[index + 1]
   return index + 1
 
 
-def quick_sort(array, start, stop):
-  if start < stop:
-    pivot = randint(start, stop)
-    array[stop], array[pivot] = array[pivot], array[stop]
-    p = partition(array, start, stop)
+def quick_sort(array, start, end):
+  if start < end:
+    pivot = randint(start, end)
+    array[end], array[pivot] = array[pivot], array[end]
+    p = partition(array, start, end)
     quick_sort(array, start, p - 1)
-    quick_sort(array, p + 1, stop)
+    quick_sort(array, p + 1, end)
   return array
 
 
